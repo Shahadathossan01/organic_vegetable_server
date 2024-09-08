8923903ec740ec3bdf02638fbcd094694acb284c
shahadat hossen <hossantopu@gmail.com>
2024-09-08 11:44:32 +0600

    update

--- db.js
+++ db.js
@@ -1,6 +1,11 @@
 const mongoose=require('mongoose')
+const error = require('./utils/error')
 const connectDB=(connectionStr)=>{
-    return mongoose.connect(connectionStr)
+    try{
+        return mongoose.connect(connectionStr)
+    }catch{
+        throw error
+    }
 }
 
 module.exports=connectDB
